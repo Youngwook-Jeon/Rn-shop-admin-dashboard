744ba071b7e15436a8e13707966c529852482df0
Youngwook-Jeon <ela87@naver.com>
2024-11-27 23:43:58 +0900

    Setup the page of categories

--- src/app/admin/categories/page.tsx
+++ src/app/admin/categories/page.tsx
@@ -1,8 +1,8 @@
 import { getCategoriesWithProducts } from "@/actions/categories";
+import CategoriesPageComponent from "./page-component";
 
 export default async function Categories() {
-    const categories = await getCategoriesWithProducts();
-    console.log(categories);
-    
-    return <>Categories</>
+  const categories = await getCategoriesWithProducts();
+
+  return <CategoriesPageComponent categories={categories} />;
 }
